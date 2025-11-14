8756486f74588b9b668053314ca99b2af14155b0
alhabib5565 <alhabib5565@gmail.com>
2025-11-14 21:35:07 +0600

    feat: implement user settings page with profile and password change form

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,13 +1,35 @@
-function Settings() {
+"use client";
+
+import ChangePasswordForm from "@/components/dashboard/settings/ChangePasswordForm";
+import ProfileForm from "@/components/dashboard/settings/ProfileForm";
+import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
+
+export default function Settings() {
   return (
-    <div className="p-6">
-      <h1 className="text-3xl font-bold mb-4">Settings</h1>
-      <p className="text-muted-foreground">
-        Configure your application settings and preferences.
-      </p>
+    <div className=" space-y-6">
+      <h1 className="text-2xl font-semibold mb-4">Account Settings</h1>
+
+      <Tabs defaultValue="profile" className="w-full">
+        {/* Tab Buttons */}
+        <TabsList className="grid grid-cols-2">
+          <TabsTrigger className="cursor-pointer" value="profile">
+            Profile
+          </TabsTrigger>
+          <TabsTrigger className="cursor-pointer" value="password">
+            Change Password
+          </TabsTrigger>
+        </TabsList>
+
+        {/* Profile Tab */}
+        <TabsContent value="profile" className="mt-4">
+          <ProfileForm />
+        </TabsContent>
+
+        {/* Change Password Tab */}
+        <TabsContent value="password" className="mt-4">
+          <ChangePasswordForm />
+        </TabsContent>
+      </Tabs>
     </div>
-  )
+  );
 }
-
-export default Settings
-
